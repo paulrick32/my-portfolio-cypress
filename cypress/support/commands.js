4c7e404ef23974dbd87604bfa8e93d5316983a2a
Paulo <pr.atg32@gmail.com>
2026-02-10 09:57:30 -0300

    updated the command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,7 @@
 Cypress.Commands.add("validateHomeTitle", () => {
   cy.visit("/");  
   cy.get("h3.sq-site-title").should("be.visible");
+  cy.contains("h3.sq-site-title a", "AcademyBugs.com")
+    .should("be.visible")
+    .and("have.attr", "href", "https://academybugs.com/");
 });
